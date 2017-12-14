063b0dda53502512e2f9d03b3011bcc7912cd3f0
Guido Manta <guido.manta@gmail.com>
2017-12-14 14:52:14 +0100

    Updated activity model

--- app/Activity.php
+++ app/Activity.php
@@ -6,14 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Activity extends Model
 {
+    const TYPE_FACEBOOK = 'Facebook';
+    const TYPE_INSTAGRAM = 'Instagram';
+    const TYPE_MYBUSINESS = 'MyBusiness';
+    const TYPE_GOOGLE = 'Google';
+
+    const VALIDATION_TYPE_WAITING = 'Waiting';
+    const VALIDATION_TYPE_PUBLISHABLE = 'Publishable';
+    const VALIDATION_TYPE_REVIEW = 'Review';
+
     protected $fillable = [
         'release_date',
         'type',
         'category',
         'text',
-        'filename',
-        'mime',
-        'original_filename',
+        'media',
         'text_validation',
         'media_validation'
     ];
@@ -23,6 +30,29 @@ class Activity extends Model
     protected $dates = [
         'release_date'
     ];
+
+    protected static $types = [
+        self::TYPE_FACEBOOK,
+        self::TYPE_INSTAGRAM,
+        self::TYPE_MYBUSINESS,
+        self::TYPE_GOOGLE
+    ];
+
+    protected static $validationTypes = [
+        self::VALIDATION_TYPE_WAITING,
+        self::VALIDATION_TYPE_PUBLISHABLE,
+        self::VALIDATION_TYPE_REVIEW
+    ];
+
+    public static function getTypes()
+    {
+        return self::$types;
+    }
+
+    public static function getValidationTypes()
+    {
+        return self::$validationTypes;
+    }
 
     public function project()
     {
